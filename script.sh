4f4ca00d3be421b76ed678cfd7fcb9ec0399d570
safalya-shahakar <safalyashahakar94@gmail.com>
2024-12-14 17:45:47 +0530

    initial commit

--- script.sh
+++ script.sh
@@ -2,3 +2,5 @@
 sudo apt update
 cd /home/ubuntu/.ssh/
 echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKymy3xMQItPgx0R/XBbyp/N9cL184zftGNSvtHpqz6b ubuntu@ip-172-31-4-130 >> authorized_keys
+
+#ssh -o StrictHostKeyChecking=no ip
